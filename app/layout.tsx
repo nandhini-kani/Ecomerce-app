bdc2780898e0378fe8b67066ff62c312dc0120d3
nandhini-kani <nandhukarthi2004@gmail.com>
2025-11-30 16:40:36 +0530

    layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
 "use client";
 
+import type { ReactNode } from "react";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import CartProvider from "@/context/CartContext";
 import WishlistProvider from "@/context/WishlistContext";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <SessionProvider>
           <CartProvider>
-            <WishlistProvider>
-              {children}
-            </WishlistProvider>
+            <WishlistProvider>{children}</WishlistProvider>
           </CartProvider>
         </SessionProvider>
       </body>
